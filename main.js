85742a1a3d726ec99e5817e6db6c7401ae5f3c7f
lunaGatica <89168675+lunaGatica@users.noreply.github.com>
2022-07-20 21:13:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const Contenedor = require('./contenedor');
+
+
+const contenedor = new Contenedor('./prueba.txt')
+//contenedor.save({nombre:'Alfajor de chocolate 1', precio:'100',categoria:'alfajores',descripcion:'Alfajores de chocolate' })
+
+//contenedor.getById(1)
+
+//contenedor.getAll()
+
+//contenedor.delete(2)
+
+//contenedor.deleteAll()
