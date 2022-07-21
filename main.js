5182a1d2f2d5ab463dc670ecce3a149c517e5ef9
lunaGatica <89168675+lunaGatica@users.noreply.github.com>
2022-07-20 21:12:26 -0300

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-const Contenedor = require('./contenedor');
-
-
-const contenedor = new Contenedor('./prueba.txt')
-contenedor.save({nombre:'Alfajor de chocolate', precio:'100',categoria:'alfajores',descripcion:'Alfajores de chocolate' })
